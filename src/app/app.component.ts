d2e097d8fe0a27c0d68376477af048a870051cf0
Maciej Treder <contact@maciejtreder.com>
2018-11-04 17:18:04 +0100

    Angular SEO with AWS Lambda - step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser , DOCUMENT} from '@angular/common';
+import { environment } from '../environments/environment';
+import { Component , OnInit, Inject, PLATFORM_ID} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularSeo';
+
+    public ngOnInit(): void {
+        if (!isPlatformBrowser(this.platformId)) {
+            let bases = this.document.getElementsByTagName('base');
+    
+            if (bases.length > 0) {
+                bases[0].setAttribute('href', environment.baseHref);
+            }
+        }
+    }
+
+
+ constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any) {}
 }
